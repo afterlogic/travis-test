4e3a5beb36eedb1a5daec022b771de62021f8c93
afterlogic <support@afterlogic.com>
2020-06-15 10:57:02 +0300

    init

---
+++ travis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TASK="build"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--task)
+    TASK="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo TASK  = "$TASK"
+
+if [ "$TASK" = "upload" ]; then
+	curl  -u ${FTP_USER}:${FTP_PASSWORD} ftp://mail.afterlogic.com
+fi
